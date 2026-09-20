3923df51df16bd640c3c64cda44bd18e067b3f72
kimos65592 <kimos65592@gmail.com>
2026-09-20 09:27:10 +0300

    Remove Android configuration from build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,19 +2,3 @@ plugins {
     id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
-android {
-    namespace = "com.ahmed.electricitycard"
-    compileSdk = 35
-    defaultConfig {
-        applicationId = "com.ahmed.electricitycard"
-        minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-    }
-}
-dependencies {
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.activity:activity-ktx:1.10.0")
-}
