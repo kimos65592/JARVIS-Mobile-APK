422a7e91d84ee44f3e6873190198bd1c9aca6665
kimos65592 <kimos65592@gmail.com>
2026-09-20 09:24:54 +0300

    Update Gradle plugins syntax for Android and Kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,12 @@
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
+
 android {
     namespace = "com.ahmed.electricitycard"
     compileSdk = 35
+
     defaultConfig {
         applicationId = "com.ahmed.electricitycard"
         minSdk = 24
@@ -13,6 +15,7 @@ android {
         versionName = "1.0"
     }
 }
+
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
